50594c03ae8168ecafd9784d8cc2fa7bef336004
Valentin <valentin.allimann@divtec.ch>
2026-08-19 11:38:13 +0200

    jalon 1 terminé

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,7 +1,24 @@
 public class YahtzeeProcedural {
 
-    public static void main(String[] args) {
-        System.out.println( "Bon courage !");
+    static int sideCount = 6;
 
+
+    // fonction pour lancer les dés
+    static int roll() {
+        return (int) (Math.random() * sideCount) + 1;
+    }
+// Point d'entrée du programme
+    public static void main(String[] args) {
+        int[] des = new int[5];
+        for (int indexDe = 0; indexDe < des.length; indexDe++) {
+            des[indexDe] = roll();
+        }
+            afficherDes(des);
+    }
+
+    public static void afficherDes(int []des ) {
+        for (int indexDe = 0; indexDe < des.length; indexDe++) {
+            System.out.println("Dé " + (indexDe + 1) + " : " + des[indexDe]);
+        }
     }
 }
