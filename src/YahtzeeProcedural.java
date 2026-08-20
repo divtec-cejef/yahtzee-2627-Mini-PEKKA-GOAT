8b1990471c89af73810a953775e00455f84510ab
Valentin <valentin.allimann@divtec.ch>
2026-08-20 13:24:03 +0200

    jalon 1 terminé

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,7 +1,10 @@
+//importer pour faire saisir des données à l'utilisateur
+import java.util.Scanner;
+
+
 public class YahtzeeProcedural {
-
+// Nombre de face des dés
     static int sideCount = 6;
-
 
     // fonction pour lancer les dés
     static int roll() {
@@ -14,8 +17,15 @@ public class YahtzeeProcedural {
             des[indexDe] = roll();
         }
             afficherDes(des);
+
+        //Choix des dés a relancer
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Quel(s) dé(s) souhaiteriez vous relancer ? ");
+        int n = reader.nextInt();
     }
 
+
+    //méthode d'affichage des dés au premier lancer
     public static void afficherDes(int []des ) {
         for (int indexDe = 0; indexDe < des.length; indexDe++) {
             System.out.println("Dé " + (indexDe + 1) + " : " + des[indexDe]);
